854a6e2d6e60d31997f7e7006e307deef388f8aa
Franklyn A. Antigua <franklynantigua@hotmail.com>
2016-11-14 23:15:24 -0700

    Got WebServices  Succesfully

---
+++ GetWebService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetWebService.WebServices;
+
+namespace GetWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServices.GlobalWeatherSoap ws= new GlobalWeatherSoapClient();
+            string listaCiudadesPeru = ws.GetCitiesByCountry("PERU");
+
+            Console.WriteLine("Las Cuidades de Peru Son :"  + listaCiudadesPeru);
+
+            Console.WriteLine("");
+
+
+            string listaCiudadesBrazil = ws.GetCitiesByCountry("BRAZIL");
+
+            Console.WriteLine("Las Cuidades de BRAZIL Son :" + listaCiudadesBrazil);
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
